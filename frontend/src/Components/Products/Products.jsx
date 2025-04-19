e7f644bdece6ee105d82c1218967fbc8e95bda38
Ifath-101 <ifathahamed01@gmail.com>
2025-04-19 12:56:30 +0530

    Add backend with Express setup

---
+++ frontend/src/Components/Products/Products.jsx
@@ -0,0 +1,14 @@
+import React, { useContext } from 'react'
+import './Products.css'
+import { ShopContext } from '../../Context/ShopContext.jsx'
+
+const Home = () => {
+    const {all_product} = useContext(ShopContext)
+    return(
+    <div>
+
+    </div>
+    )
+}
+
+export default Home
